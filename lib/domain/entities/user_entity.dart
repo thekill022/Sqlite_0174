189598c7af14479f30b07481b5c4c9753816dbbb
thekill022 <gibfathoni@gmail.com>
2026-04-24 21:15:02 +0700

    Menambahkan NoTelepon dan alamat di entity

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -2,6 +2,8 @@ class UserEntity {
   final String id;
   final String name;
   final String email;
+  final String NoTelepon;
+  final String alamat;
 
-  UserEntity({required this.id, required this.name, required this.email});
+  UserEntity({required this.id, required this.name, required this.email, required this.NoTelepon, required this.alamat});
 }
