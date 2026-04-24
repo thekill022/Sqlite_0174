a50bd9231eded069b8f24fd0c55dc4ffb01a7eac
thekill022 <gibfathoni@gmail.com>
2026-04-24 21:18:07 +0700

    Menambahkan NoTelepon dan alamat di user model

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,13 +1,13 @@
 import 'package:meet8/domain/entities/user_entity.dart';
 
 class UserModel extends UserEntity {
-  UserModel({required super.id, required super.name, required super.email});
+  UserModel({required super.id, required super.name, required super.email, required super.NoTelepon, required super.alamat});
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'name': name, 'email': email};
+    return {'id': id, 'name': name, 'email': email, 'NoTelepon' : NoTelepon, 'alamat' : alamat};
   }
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
-    return UserModel(id: map['id'], name: map['name'], email: map['email']);
+    return UserModel(id: map['id'], name: map['name'], email: map['email'], NoTelepon: map['NoTelepon'], alamat: map['alamat']);
   }
 }
