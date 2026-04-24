8d6190ca3a9fdf8471c4c001969a38b2bad456c4
thekill022 <gibfathoni@gmail.com>
2026-04-24 10:25:30 +0700

    membuat even untuk CRUD data user

--- lib/bloc/user_event.dart
+++ lib/bloc/user_event.dart
@@ -1,4 +1,20 @@
-part of 'user_bloc.dart';
+import 'package:meet8/domain/entities/user_entity.dart';
 
-@immutable
-sealed class UserEvent {}
+abstract class UserEvent {}
+
+class LoadUsers extends UserEvent {}
+
+class AddUserEvent extends UserEvent {
+  final UserEntity user;
+  AddUserEvent(this.user);
+}
+
+class DeleteUserEvent extends UserEvent {
+  final String id;
+  DeleteUserEvent(this.id);
+}
+
+class UpdateUserEvent extends UserEvent {
+  final UserEntity user;
+  UpdateUserEvent(this.user);
+}
