1c2a388262f2e195536ec49dc26f122628408f2c
thekill022 <gibfathoni@gmail.com>
2026-04-24 11:13:48 +0700

    membuat user form page dan menyertakan user entity ke konstruktor class

---
+++ lib/pages/user_form.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:meet8/domain/entities/user_entity.dart';
+
+class UserFormPage extends StatefulWidget {
+  final UserEntity? user;
+
+  const UserFormPage({super.key, this.user});
+
+  @override
+  State<UserFormPage> createState() => _UserFormPageState();
+}
+
+class _UserFormPageState extends State<UserFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(children: []),
+      ),
+    );
+  }
+}
