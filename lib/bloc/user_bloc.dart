05b6b08b1a57bff9e326241f0ba443be23b660ce
thekill022 <gibfathoni@gmail.com>
2026-04-24 10:38:19 +0700

    membuat method bloc untuk CRUD data user

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -1,13 +1,33 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'user_event.dart';
-part 'user_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meet8/bloc/user_event.dart';
+import 'package:meet8/bloc/user_state.dart';
+import 'package:meet8/data/repository/user_repository.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserInitial()) {
-    on<UserEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserRepository repository;
+
+  UserBloc(this.repository) : super(UserInitial()) {
+    on<LoadUsers>((event, emit) async {
+      emit(UserLoading());
+      try {
+        final user = await repository.getAllUsers();
+        emit(UserLoaded(user));
+      } catch (e) {
+        emit(UserError("Gagal memuat data"));
+      }
+    });
+
+    on<AddUserEvent>((event, emit) async {
+      await repository.addUser(event.user);
+      add(LoadUsers());
+    });
+    on<UpdateUserEvent>((event, emit) async {
+      await repository.updateUser(event.user);
+      add(LoadUsers());
+    });
+    on<DeleteUserEvent>((event, emit) async {
+      await repository.deleteUser(event.id);
+      add(LoadUsers());
     });
   }
 }
