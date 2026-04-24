a4d23da58e2b7c2ff8968dee2900b145a4446f3c
thekill022 <gibfathoni@gmail.com>
2026-04-24 10:49:47 +0700

    membuat database helper untuk inisialisasi db dan membuat table

---
+++ lib/domain/helper/database_helper.dart
@@ -0,0 +1,29 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static Database? _database;
+
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDb();
+    return _database!;
+  }
+
+  Future<Database> _initDb() async {
+    String path = join(await getDatabasesPath(), 'user_database.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE users(id TEXT PRIMARY KEY, name TEXT, email TEXT)",
+        );
+      },
+    );
+  }
+}
